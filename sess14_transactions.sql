bc000b11cd3a3186df8259b442867f98e8122a06
mac <mac@macs-MacBook-Pro.local>
2026-05-26 12:00:04 +0300

    Complete T-SQL demonstration on how to work with transaction and begin error handling

--- sess14_transactions.sql
+++ sess14_transactions.sql
@@ -1,6 +1,5 @@
 /*Session 1 4covers transactions in sql server*/
-CREATE DATABASE Customer_DB_ADSE2509;
-GO
+
 --switch to the customer database
 Use Customer_DB_ADSE2509
 
@@ -18,6 +17,7 @@ begin transaction @transName;
 delete from dbo.EmployeeDetails
 where EmpID = 106;
 
+
 --Begin a transaction to delete James' details from the employeeDetails table
 Declare @delsName nvarchar(30) = 'deleteJames';
 begin transaction @deleteJames;
@@ -29,8 +29,6 @@ create table ValueTable
 (
   [Value] nchar not null
 );
-
- 
 
 -- Add/insert values into the ValueTable using explicit transactions
 Begin tran
@@ -45,22 +43,76 @@ Begin tran
 -- Undo the inserts
 Rollback tran
 
---Create a custom store procedure with a savepoint
+--create a custom 
 create proc uspSaveTransExample
 @inputCandidateID int
 as 
 Declare @transCounter int;
 set @transCounter = @@TranCount;
 if @transCounter > 0 
-	Save tran ProcedureSave;
+  Save tran ProcedureSave;
 else
-	Begin Tran;
-		Delete from
-		AdventureWorks2022.HumanResources.JobCandidate
-		where JobCandidateID = @inputCandidateID;
-		if @transCounter = 0
-		print 'Transaction successful!'
-		commit tran;
-		if @transCounter = 1
-		print 'Transaction rolled back!'
-	Rollback tran ProcedureSave;
+  Begin Tran;
+    Delete from
+    AdventureWorks2022.HumanResources.JobCandidate
+    where JobCandidateID = @inputCandidateID;
+    if @transCounter = 0
+    print 'Transaction successful!'
+    commit tran;
+    if @transCounter = 1
+    print 'Transaction rolled back!'
+  Rollback tran ProcedureSave;
+
+--the existing  upSaveTransExample 
+drop proc uspSaveTransExample;
+
+CREATE PROC uspSaveTransExample
+@inputCandidateID INT
+AS
+BEGIN
+    DECLARE @transCounter INT;
+    SET @transCounter = @@TRANCOUNT;
+ 
+    IF @transCounter > 0
+    BEGIN
+        SAVE TRAN ProcedureSave;
+    END
+    ELSE
+    BEGIN
+        BEGIN TRAN;
+    END
+ 
+    BEGIN TRY
+        DELETE FROM AdventureWorks2022.HumanResources.JobCandidate
+        WHERE JobCandidateID = @inputCandidateID;
+ 
+        IF @transCounter = 0
+        BEGIN
+            COMMIT TRAN;
+            PRINT 'Transaction committed successfully.';
+        END
+        ELSE
+        BEGIN
+            PRINT 'Savepoint created and DELETE successful inside existing transaction.';
+        END
+    END TRY
+    BEGIN CATCH
+        IF @transCounter = 0
+        BEGIN
+            ROLLBACK TRAN;
+            PRINT 'Transaction rolled back due to error.';
+        END
+        ELSE
+        BEGIN
+            ROLLBACK TRAN ProcedureSave;
+            PRINT 'Rolled back to savepoint due to error.';
+        END        
+    END CATCH
+END;
+
+--Execute the above stored procedure to remove employee number 13
+exec upSaveTransExample;
+
+Demonstrate the use of @@trancount function in nested begin and commit statements
+
+
